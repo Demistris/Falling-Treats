1e907034fd1c1e6ba9c4c4ff251102daf75b26c5
Kaja Więckowska <k.wieckowska66@gmail.com>
2020-01-02 23:01:59 +0100

    I saved the items I bought

--- Assets/Scripts/ItemStats.cs
+++ Assets/Scripts/ItemStats.cs
@@ -6,26 +6,38 @@ using TMPro;
 
 public class ItemStats : MonoBehaviour
 {
+    public int Id;
     public int Price;
     public TextMeshProUGUI PriceText;
     public GameObject Texts;
-    public bool isItemBought = false;
+    public bool isItemBought
+    {
+        get { return PlayerPrefs.GetInt("BoughtItem" + Id) == 1 ? true : false; }
+        set { PlayerPrefs.SetInt("BoughtItem" + Id, value ? 1 : 0); }
+    }
 
     private Image image;
 
     void Start()
     {
         PriceText.text = Price.ToString();
+        RefreshBoughtState();
     }
 
-    public void BuyingItems()
+    public void BuyItems()
     {
-        if(PlayerStats.Diamonds >= Price)
+        if (PlayerStats.Diamonds >= Price)
         {
             isItemBought = true;
             PlayerStats.Diamonds -= Price;
+            PlayerPrefs.SetInt("Diamonds", PlayerStats.Diamonds);
         }
 
+        RefreshBoughtState();
+    }
+
+    private void RefreshBoughtState()
+    {
         if (isItemBought == true)
         {
             image = GetComponent<Image>();
