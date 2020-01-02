5ae93fbdd804b4e78a7b968455492e4140e8bdce
Kaja Więckowska <k.wieckowska66@gmail.com>
2020-01-02 01:02:51 +0100

    Updated Shop

---
+++ Assets/Scripts/ChangePlayerSprite.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlayerSprite : MonoBehaviour
+{
+    public GameObject Player;
+    public ItemStats ItemStats;
+
+    public void Change(Sprite differentSprite)
+    {
+        if(ItemStats.isItemBought == true)
+        {
+            Player.GetComponent<SpriteRenderer>().sprite = differentSprite;
+        }
+    }
+}
---
+++ Assets/Scripts/ItemStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemStats : MonoBehaviour
+{
+    public int Price;
+    public TextMeshProUGUI PriceText;
+    public GameObject Texts;
+    public bool isItemBought = false;
+
+    private Image image;
+
+    void Start()
+    {
+        PriceText.text = Price.ToString();
+    }
+
+    public void BuyingItems()
+    {
+        if(PlayerStats.Diamonds >= Price)
+        {
+            isItemBought = true;
+            PlayerStats.Diamonds -= Price;
+        }
+
+        if (isItemBought == true)
+        {
+            image = GetComponent<Image>();
+            image.color = Color.white;
+            var tempColor = image.color;
+            tempColor.a = 0.862f;
+            image.color = tempColor;
+
+            Texts.SetActive(false);
+        }
+    }
+}
